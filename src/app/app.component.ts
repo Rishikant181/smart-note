f1356e747ee7a3a6a8cc47d9c1f47d6be22e97d4
Rishikant Sahu <rishikantsahu181@gmail.com>
2021-10-09 20:30:01 +0530

    Made changes to toolbar to handle changing of login status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'smart-note';
+    title: string = 'smart-note';
+    toolbarTitle: string;                                              // To store text to be shown on toolbar title
+    loggedIn: boolean;                                                 // To store whether user is looged in or not
+
+    // The constructor
+    constructor() {
+        this.toolbarTitle = "Smart Notes";
+        this.loggedIn = false;
+    }
+
+    // Method to show login dialog
+    showLoginDialog(): void {
+        console.log("Login")
+    }
 }
