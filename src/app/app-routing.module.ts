63d3c03470eadefce5a6d233714af99c85fd893b
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-01-01 07:34:41 +0530

    Added a basic authentication guard
    The authentication guard prevents unauthorized access to certain parts
    of the application, if the user is not logged in. For now, it return
    true all the time

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountCreationComponent } from './account-creation/account-creation.component';
 
+import { AuthGuard } from './guards/auth/auth.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
     { path: 'home', component: HomePageComponent },
     { path: 'accounts/create', component: AccountCreationComponent },
-    { path: 'dashboard', component: DashboardComponent},
+    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
